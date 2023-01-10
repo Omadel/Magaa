8869cd658210d2ed3e3a2be985775c5a2af3e1b9
Etienne Javelle <83096541+Omadel@users.noreply.github.com>
2023-01-10 22:49:31 +0100

    [Add] better joystick

--- VampireClone/Assets/_Project/Scripts/Runtime/Joystick.cs
+++ VampireClone/Assets/_Project/Scripts/Runtime/Joystick.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -8,65 +9,72 @@ namespace VampireClone
 {
     public class Joystick : MonoBehaviour
     {
-        private Image touchDelta;
+        [SerializeField] private float fadeDuratiuon = .1f;
+
+        private Image touchPosition;
         private bool shouldUpdateJoystick;
         private RectTransform rectTransform;
         private CanvasGroup canvasGroup;
         private List<RaycastResult> raycastResults = new List<RaycastResult>();
+        private Tween fadeTween;
 
         private void Awake()
         {
-            canvasGroup = gameObject.AddComponent<CanvasGroup>();
+            canvasGroup = GetComponent<CanvasGroup>();
+            if (canvasGroup == null) canvasGroup = gameObject.AddComponent<CanvasGroup>();
         }
 
         private void Start()
         {
             rectTransform = GetComponent<RectTransform>();
             Player.Instance.SetJoystick(this);
-            touchDelta = transform.GetChild(0).GetComponent<Image>();
+            touchPosition = transform.GetChild(0).GetComponent<Image>();
             Disable();
         }
 
-        private void UpdateTouchDelta(Vector2 position)
+        private void ProcessTouchPosition(Vector2 position)
         {
-            // Calculate the direction from the transform's position to the touch position
-            Vector3 direction = (position - (Vector2)transform.position).normalized;
+            // Return if not enabled
+            if (!enabled) return;
+
+            // Calculate direction from touch position to the transform's position
+            Vector3 touchDirection = (position - (Vector2)transform.position).normalized;
             // Scale the direction by half the size of the rectTransform
-            direction *= (rectTransform.rect.size * .5f);
-            // Calculate the clamped position
-            //Vector2 clampedPosition = ClampPositionWithinBounds(transform.position + direction);
-            Vector2 clampedPosition = transform.position + direction;
-            // Update the touchDelta's position
-            touchDelta.transform.position = clampedPosition;
-            // Set the direction for the player
-            Player.Instance.SetDirection(direction.normalized);
+            Vector2 scaledDirection = touchDirection * rectTransform.rect.size * .5f; ;
+            // Clamp the touch position to the min/max bounds of the rectTransform
+            Vector2 clampedPosition = ClampToCircle(position, transform.position, rectTransform.rect.size.x * .5f);
+            // Set the position of the touchDelta object
+            touchPosition.transform.position = clampedPosition;
+
+            // Set the direction on the Player instance
+            Player.Instance.SetDirection(scaledDirection.normalized);
         }
 
-        private Vector2 ClampPositionWithinBounds(Vector2 position)
+        private Vector3 ClampToCircle(Vector3 position, Vector3 center, float radius)
         {
-            Vector2 min = (Vector2)transform.position - rectTransform.rect.size * .5f;
-            Vector2 max = (Vector2)transform.position + rectTransform.rect.size * .5f;
-            return new Vector2(Mathf.Clamp(position.x, min.x, max.x),
-                               Mathf.Clamp(position.y, min.y, max.y));
+            Vector3 direction = position - center;
+            bool isInsideTheCircle = direction.magnitude <= radius;
+            return isInsideTheCircle ? position : center + (direction.normalized * radius);
         }
 
-        private void UpdateJoystick(Vector2 position)
+        private bool TryEnablejoystick(Vector2 position)
         {
             if (EventSystem.current == null)
             {
                 Debug.LogError("EventSystem.current is null");
-                return;
+                return false;
             }
             PointerEventData eventData = new PointerEventData(EventSystem.current) { position = position };
             raycastResults.Clear();
             EventSystem.current.RaycastAll(eventData, raycastResults);
             bool blockingUIObjectHit = raycastResults.Count > 0;
-            if (blockingUIObjectHit) return;
+            if (blockingUIObjectHit) return false;
 
             shouldUpdateJoystick = false;
             transform.position = position;
-            touchDelta.transform.localPosition = Vector3.zero;
+            touchPosition.transform.localPosition = Vector3.zero;
             Enable();
+            return true;
         }
 
         public void Press(InputValue input)
@@ -79,21 +87,25 @@ namespace VampireClone
 
         private void Disable()
         {
-            canvasGroup.alpha = 0f;
+            fadeTween?.Complete();
+            fadeTween = canvasGroup.DOFade(0, fadeDuratiuon);
             Player.Instance.SetDirection(Vector2.zero);
+            enabled = false;
         }
 
         private void Enable()
         {
-            canvasGroup.alpha = 1f;
+            fadeTween?.Complete();
+            fadeTween = canvasGroup.DOFade(1f, fadeDuratiuon);
+            enabled = true;
         }
 
         internal void ChangePosition(Vector2 position)
         {
             if (EventSystem.current.currentSelectedGameObject != null) return;
 
-            if (shouldUpdateJoystick) UpdateJoystick(position);
-            else UpdateTouchDelta(position);
+            if (shouldUpdateJoystick) TryEnablejoystick(position);
+            else ProcessTouchPosition(position);
         }
     }
 }
